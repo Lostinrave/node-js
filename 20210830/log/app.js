848773920890a3f5ed4cc43af787d9e1756fd408
Lostinrave <83598946+Lostinrave@users.noreply.github.com>
2021-09-02 18:49:35 +0300

    finished exercise

--- 20210830/log/app.js
+++ 20210830/log/app.js
@@ -1,13 +1,23 @@
 const express=require('express'); // Prisidedam biblioteka
+const path=require('path');
+const hbs=require('hbs'); // template engine
+
 const pageRouter=require('./routes/page');
 const logRouter=require('./routes/log');
-const path=require('path');
-const app=express(); // sukuriam objekta
-app.set('view engine','hbs');
 
+
+const viewsPath=path.join(__dirname,'views','templates');
+const partialsPath=path.join(__dirname,'views','partials');
+const publicPath=path.join(__dirname,'public');
+
+const app=express(); // sukuriam objekta
+
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname, 'public'))); //registruojam express.static middleware kuris pagal uzklausas atsius failus is katalogo kuri mes nurodeme kaip kintamaji
+app.use(express.static(publicPath)); //registruojam express.static middleware kuris pagal uzklausas atsius failus is katalogo kuri mes nurodeme kaip kintamaji
 app.use(logRouter);
 app.use(pageRouter);
 
--- 20210830/users/app.js
+++ 20210830/users/app.js
@@ -1,10 +1,12 @@
 const express=require('express'); // Prisidedam biblioteka
 const path = require('path');
-const hbs=require('hbs');
+const hbs=require('hbs'); // template engine
+
 //routes
 const systemRouter=require('./routes/system');
 const pageRouter=require('./routes/page');
 const userRouter=require('./routes/user');
+
 //keliai
 //konstanta, kelias iki sablonu
 const viewsPath=path.join(__dirname,'views','templates');
@@ -13,11 +15,13 @@ const publicPath=path.join(__dirname,'public');
 
 // sukuriam objekta
 const app=express(); 
-//nustatymai turi buti pries middleware bet po objekto
 
+//nustatymai turi buti pries middleware bet po objekto
+//pakeicia globalius nustatymus
 app.set('view engine','hbs');
 app.set('views',viewsPath);
 hbs.registerPartials(partialsPath);
+
  // body-parser, issplitina inputo stringa i objekta, kad galetume pasiimti inputo reiksme
 app.use(express.urlencoded({extended:false}));
 //registruojam express.static middleware kuris pagal uzklausas atsius failus is katalogo kuri mes nurodeme kaip kintamaji
